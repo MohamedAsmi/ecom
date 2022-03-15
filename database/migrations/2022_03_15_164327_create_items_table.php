9f56a6b96c6a929eaab1443488a38674cec18d35
hakeemabd <hakeemabd88@gmail.com>
2022-03-15 23:14:22 +0530

    create_new model,item,item_cat

---
+++ database/migrations/2022_03_15_164327_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name');
+            $table->double('item_price');
+            $table->integer('item_quantity');
+            $table->string('item_image');
+            $table->double('item_offer');
+            $table->integer('item_cat_id');
+            $table->timestamps();
+
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
