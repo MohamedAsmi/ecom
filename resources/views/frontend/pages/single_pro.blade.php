4140e5f4cf048034d35cf2755af5119569d9e36b
abdhakeem18 <071husain484@gmail.com>
2022-03-15 08:41:01 +0530

    single product page update

--- resources/views/frontend/pages/single_pro.blade.php
+++ resources/views/frontend/pages/single_pro.blade.php
@@ -7,14 +7,18 @@
 
 @foreach($single_shops as $data)
 <!-- Single shop Banner  -->
-<div class="col-lg-4 col-md-4 col-6">
-    <div class="single-banner">
-        <img src="{{asset($data->shop_image)}}" alt="{{$data->shop_image}}">
-        <div class="content">
-            <p>{{$data->service_type}}</p>
-            <h3>{{$data->company_name }}</h3>
-            <a href="#">Shop Now</a>
+<div class="col-12">
+    <h2 class="single_product_image">Our Package</h2>
+    <div class="single-banner row">
+        @php
+        $services = explode('|*|',$data->details_of_service);
+        $i = 0;
+        @endphp
+        @foreach($services as $service)
+        <div class="col-lg-4 col-md-4 col-6 single_product_sty" attr="{{$i++}}">
+            <div class="signle_product_ss">{{$service}}</div>
         </div>
+        @endforeach
     </div>
 </div>
 <!-- /End Single shop Banner  -->
@@ -68,3 +72,71 @@
 
 @include('frontend.layouts.newsletter')
 @endsection
+
+@push('styles')
+<style>
+    .signle_product_ss {
+        width: 84%;
+        margin: auto;
+        background: #ededed;
+        min-height: 160px;
+        padding: 70px;
+        color: black;
+        font-size: 30px;
+        font-family: inherit;
+        font-weight: 500;
+    }
+
+    .single_product_sty {
+        padding: 30px 0px;
+        text-align: center;
+        width: 100%;
+
+    }
+
+    .selected_item {
+        width: 102%;
+        margin-left: -2%;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+
+    .single_product_image{
+        font-family: fangsong;
+        text-align: center;
+        width: 100%;
+        padding: 30px 0px;
+    }
+</style>
+@endpush
+@push('scripts')
+
+<script>
+    $(document).ready(function() {
+        auto_shadow();
+
+        function auto_shadow_timer(counter) {
+            var len = $('.single_product_sty').map(function() {
+                return $(this).attr('attr');
+            });
+            var timer = setInterval(function() {
+                $('.signle_product_ss').removeClass('selected_item');
+                $('.single_product_sty[attr=' + counter + '] .signle_product_ss').addClass('selected_item');
+                counter++;
+                if (counter >= len.length) {
+                    clearInterval(timer);
+                }
+                if (counter == len.length) {
+                console.log('hello');
+                auto_shadow();
+            }
+            }, 5000);
+        
+        }
+
+        function auto_shadow() {
+            var counter = 0;
+            auto_shadow_timer(counter);
+        }
+    })
+</script>
+@endpush
