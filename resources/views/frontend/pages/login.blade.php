149554e8509c44d9938185fb207ab94078c16d5d
Afsal <afsalpx@gmail.com>
2022-03-27 21:39:51 +0530

    design chnges

--- resources/views/frontend/pages/login.blade.php
+++ resources/views/frontend/pages/login.blade.php
@@ -24,7 +24,7 @@
      <section class="shop login section">
         <div class="container">
             <div class="row"> 
-            <!-- <div class="container register">
+             <div class="container register">
                 <div class="row">
                     <div class="col-md-3 register-left">
                         <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
@@ -48,7 +48,7 @@
                                     <div class="col-md-6">
                                     <div class="form-group">
                                             <input type="email" class="form-control" placeholder="Your Email *" value="" />
-                                        </div> -->
+                                        </div> 
                                         <!-- <div class="form-group">
                                             <input type="text" class="form-control" placeholder="Email *" value="" />
                                         </div> -->
@@ -65,12 +65,12 @@
                                                 </label>
                                             </div>
                                         </div> -->
-                                    <!-- </div>
+                                    </div>
                                     <div class="col-md-6">
                                         
                                         <div class="form-group">
                                             <input type="password" class="form-control" placeholder="Password *" value="" />
-                                        </div> -->
+                                        </div> 
                                         <!-- <div class="form-group">
                                             <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
                                         </div> -->
@@ -85,7 +85,7 @@
                                         <!-- <div class="form-group">
                                             <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
                                         </div> -->
-                                        <!-- <input type="submit" class="btnRegister"  value="Register"/>
+                                         <input type="submit" class="btnRegister"  value="Register"/>
                                     </div>
                                 </div>
                             </div>
@@ -114,7 +114,7 @@
                                             <input type="text" class="form-control" placeholder="`Answer *" value="" />
                                         </div> -->
                                         
-                                        <!-- <input type="submit" class="btnRegister"  value="Register"/>
+                                         <input type="submit" class="btnRegister"  value="Register"/>
                                     </div>
                                 </div>
                             </div>
@@ -122,13 +122,13 @@
                     </div>
                 </div>
 
-            </div> -->
-               <div class="col-lg-6 offset-lg-3 col-12">
+            </div> 
+               <!-- <div class="col-lg-6 offset-lg-3 col-12">
                     <div class="login-form">
                         <h2>Login</h2>
-                        <p>Please register in order to checkout more quickly</p> 
+                        <p>Please register in order to checkout more quickly</p>  -->
                         <!-- Form -->
-                        <form class="form" method="post" action="{{route('login.submit')}}">
+                        <!-- <form class="form" method="post" action="{{route('login.submit')}}">
                             @csrf
                             <div class="row">
                                 <div class="col-12">
@@ -169,7 +169,7 @@
                                     @endif
                                 </div>
                             </div>
-                        </form> 
+                        </form>  -->
                         <!--/ End Form -->
                     </div>
                 </div>
@@ -181,7 +181,7 @@
     <!--/ End Login -->
 @endsection
 @push('styles')
-<style>
+<!-- <style>
     .shop.login .form .btn{
         margin-right:0;
     }
@@ -205,5 +205,5 @@
     .btn-google:hover{
         background:rgb(243, 26, 26) !important;
     }
-</style>
+</style> -->
 @endpush
